f97280e4db6dce04d5d96a9d6d3900a9aa6c9aa5
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-02-27 16:17:21 -0300

    feat(ui): implement reusable Button component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,14 @@
 import { Image, View } from "react-native";
 
+import { Button } from "@/components/Button";
+
 import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} />
+      <Button />
     </View>
   );
 }
