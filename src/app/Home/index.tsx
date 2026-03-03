c8ebccc800c9df8920250a2e277fc218f2718bf2
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-03-03 18:49:35 -0300

    feat: allow button customization via props

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -8,7 +8,7 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} />
-      <Button />
+      <Button title="Figure up" />
     </View>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,19 @@
-import { Text, TouchableOpacity } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  type TouchableOpacityProps,
+} from "react-native";
 
 import { styles } from "./styles";
 
-export function Button() {
+type ButtonProps = TouchableOpacityProps & {
+  title: string;
+};
+
+export function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.8}>
-      <Text style={styles.title}>Figure up</Text>
+    <TouchableOpacity style={styles.container} activeOpacity={0.8} {...rest}>
+      <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   );
 }
