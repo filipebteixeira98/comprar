dd952ee48d8823a53f0c529d832f2f7efd6cc450
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-03-18 10:17:54 -0300

    feat: implement in-memory item storage

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { Button } from "@/components/Button";
 import { Filter } from "@/components/Filter";
@@ -12,33 +19,36 @@ import { styles } from "./styles";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
-const ITEMS = [
-  {
-    id: "1",
-    status: FilterStatus.DONE,
-    description: "Buy milk",
-  },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: "Buy bread",
-  },
-  {
-    id: "3",
-    status: FilterStatus.PENDING,
-    description: "Buy eggs",
-  },
-];
-
 export function Home() {
+  const [items, setItems] = useState<any[]>([]);
+  const [description, setDescription] = useState("");
   const [filter, setFilter] = useState(FilterStatus.PENDING);
+
+  function handleIncreaseItems() {
+    if (!description.trim()) {
+      Alert.alert("Error", "Please enter a valid item description.");
+
+      return;
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+
+    setItems((previousState) => [...previousState, newItem]);
+  }
 
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} />
       <View style={styles.form}>
-        <Input placeholder="What do you need to buy?" />
-        <Button title="Figure up" />
+        <Input
+          placeholder="What do you need to buy?"
+          onChangeText={setDescription}
+        />
+        <Button title="Figure up" onPress={handleIncreaseItems} />
       </View>
       <View style={styles.content}>
         <View style={styles.header}>
@@ -55,7 +65,7 @@ export function Home() {
           </TouchableOpacity>
         </View>
         <FlatList
-          data={ITEMS}
+          data={items}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <Item
